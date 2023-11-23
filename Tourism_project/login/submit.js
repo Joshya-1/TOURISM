aa48e3fedc72aeb7777e9645aab7e55543cac97b
JoshyaManoharan <145587755+Joshya-1@users.noreply.github.com>
2023-11-23 21:01:25 +0530

    Add files via upload

---
+++ Tourism_project/login/submit.js
@@ -0,0 +1,14 @@
+// const button1 =document.querySelector('button')
+// button1.addEventListener("click",Touchhere)
+// let resultdiv=document.createElement('div')
+// resultdiv.id="result"
+// document.getElementById("citydiv").appendChild(resultdiv)
+// function Touchhere(){
+//     const dropdownbox=document.getElementById("dropdownbox")
+//     const tour=dropdownbox.options[dropdownbox.selectedIndex].value
+//     let one=0
+//     switch(tour){
+//         case 'loc1':
+//             link
+//     }
+// }
